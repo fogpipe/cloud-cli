846083e92bd7d39f12de160ef2994d343dbb4963
Lorentz Lasson <lasson.lorentz@gmail.com>
2026-09-10 11:45:59 +0200

    fpcloud v0.181.0

--- package.nix
+++ package.nix
@@ -18,7 +18,7 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "fpcloud";
-  version = "0.180.2"; # stamped into the tagged commit by the workspace release recipe
+  version = "0.181.0"; # stamped into the tagged commit by the workspace release recipe
 
   src = ./.;
 
