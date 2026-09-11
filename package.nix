3fa930c7be34802e0d1194f7b8bcf7b36303dd2b
Alexander Félix <alexander@fogpipe.com>
2026-09-11 11:37:47 +0200

    fpcloud v0.184.2

--- package.nix
+++ package.nix
@@ -18,7 +18,7 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "fpcloud";
-  version = "0.184.0"; # stamped into the tagged commit by the workspace release recipe
+  version = "0.184.2"; # stamped into the tagged commit by the workspace release recipe
 
   src = ./.;
 
